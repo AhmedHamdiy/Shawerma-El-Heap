a8f0d6778507b603c4adf7f1b00a9c1c7a74f69c
[Abdelruhman Sami] <[abdelruhamn.mohamad04@eng-stu.cu.edu.eg]>
2023-08-30 15:50:36 +0300

    Home Page, About Page, and Title component have been finished

--- ShawewrmaElHeap/App.js
+++ ShawewrmaElHeap/App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Home from "./Pages/Home";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { About } from "./Pages/About";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Irish-Grover": require("./fonts/IrishGrover-Regular.ttf"),
+    Inika: require("./fonts/Inika-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <About />
     </View>
   );
 }
@@ -13,8 +34,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+  },
+
+  text: {
+    fontFamily: "Irish-Grover",
   },
 });
